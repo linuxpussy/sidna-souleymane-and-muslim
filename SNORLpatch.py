c47db31d44baf1dd101344afa31d937622ca22f7
linuxpussy <63728887+linuxpussy@users.noreply.github.com>
2020-05-15 16:51:06 +0200

    PATCH
    The patch for this

---
+++ SNORLpatch.py
@@ -0,0 +1,22 @@
+print ("Launching patch....")
+time.sleep(5)
+alia = num = input("Type of bug:")
+ppsi = num = input("Bug Error Number:")
+if alia == "I dont know":
+ print ("Theres IDiot! type of bug")
+ print ("also other but only IDiot!")
+else:
+     print ("ok.")
+     print ("Sending TYPE OF BUG...")
+     uu = alia
+
+
+if ppsi == "44R6T8Y":
+ print ("Invalid Number")
+ print ("Theres only K565RT")
+else:
+     print ("OK.")
+     print ("Sending BUG ERROR NUMBER")
+
+ 
+
