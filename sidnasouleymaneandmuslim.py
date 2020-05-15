249b07b3b4f1a216c6a4afea4edac6921bb908f5
linuxpussy <63728887+linuxpussy@users.noreply.github.com>
2020-05-15 16:59:48 +0200

    Create sidnasouleymaneandmuslim.py

---
+++ sidnasouleymaneandmuslim.py
@@ -0,0 +1,4 @@
+print ("SIDNA SOULEYMANE can control bad or good djinns, talk to ANIMALS, he haves a big army, he is a king")
+print ("QURAN")
+print ("HOUHOUD OF SOULEYMANE")
+print ("NAHLA OF SOULEYMANE")
